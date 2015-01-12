d7edc92483f78fbf4bdd80bfb7a8a2a7195c7b11
JuanCarlosFist <roger0durand@gmail.com>
2015-01-12 03:00:42 -0500

    Um Craft system
    Might change dictionnary to list according to Emile's suggestion pls no
    tear apart

---
+++ craft.py
@@ -0,0 +1,92 @@
+#crafting system
+inv_m1_qtty = 10
+inv_m2_qtty = 10
+inv_m3_qtty = 10
+inv_g1_qtty = 10
+inv_g2_qtty = 10
+inv_g3_qtty = 10
+metal_type = 0
+metal_qtty = 0
+metal_1_qtty = 0
+metal_2_qtty = 0
+metal_3_qtty = 0
+gem_type = 0
+gem_qtty = 0
+gem_1_qtty = 0
+gem_2_qtty = 0
+gem_3_qtty = 0
+print 'Wat metal type??'
+while True:
+    try:
+        metal_type = input()
+    except:
+        print '1 2 or 3'
+    else:
+        if metal_type == 1 or metal_type == 2 or metal_type == 3:
+            break
+        else:
+            print 'Tas un ptit zizi  1 2 ou 3'
+            pass
+print 'How much metal'
+while True:
+    try :
+        metal_qtty = input()
+    except:
+        print '1 2 or 3'
+    else:
+        if metal_qtty == 1 or metal_qtty == 2 or metal_qtty == 3:
+            break
+        else:
+            print '1 2 or 3 n00blord'
+            pass
+if metal_type == 1:
+    metal_1_qtty = metal_qtty
+    inv_m1_qtty = inv_m1_qtty - metal_qtty
+elif metal_type == 2:
+        metal_2_qtty = metal_qtty
+        inv_m2_qtty = inv_m2_qtty - metal_qtty
+elif metal_type == 3:
+        metal_3_qtty = metal_qtty
+        inv_m3_qtty = inv_m3_qtty - metal_qtty
+#^set le type et le nombre de metal
+#mtn gems
+print 'Wat gem type??'
+while True:
+    try:
+        gem_type = input()
+    except:
+        print '1 2 or 3'
+    else:
+        if gem_type == 1 or gem_type == 2 or gem_type == 3:
+            break
+        else:
+            print 'Tas un ptit zizi  1 2 ou 3'
+            pass
+print 'How many gems?'
+while True:
+    try :
+        gem_qtty = input()
+    except:
+        print '1 2 or 3'
+    else:
+        if gem_qtty == 1 or gem_qtty == 2 or gem_qtty == 3:
+            break
+        else:
+            print '1 2 or 3 n00blord'
+            pass
+if gem_type == 1:
+    gem_1_qtty = gem_qtty
+    inv_g1_qtty = inv_g1_qtty - gem_qtty
+elif metal_type == 2:
+    gem_2_qtty = gem_qtty
+    inv_g2_qtty = inv_g2_qtty - gem_qtty
+elif metal_type == 3:
+    gem_3_qtty = gem_qtty
+    inv_g3_qtty = inv_g3_qtty - gem_qtty
+#En bas: tree prototype
+dict = {1111 : 'pistol', 1211: 'shotgun', 1311: 'potato launcher'}
+
+
+print dict[metal_type*1000 + metal_qtty*100 + 10*gem_type + gem_qtty]
+#we can add more possibilities (more metals, more gems, more stuff (rocks? idk) just need to edit the recipes
+
