d77157250f2c31c61dc311089e85c93760f26f90
henrickd <henrickdavis@yahoo.ca>
2015-01-20 13:40:20 -0500

    Stuff
    Added render surface that is blitted on screen
    Made crafting better

--- src/craft.py
+++ src/craft.py
@@ -1,6 +1,7 @@
 #crafting system
 import pygame, sys, os
 from pygame.locals import *
+display_res = (1000,600)
 type_list = {K_1: 1, K_2 : 2, K_3 : 3, K_4 :1, K_5 : 2, K_6 : 3, K_q : 1, K_w :2, K_e : 3, K_r : 1, K_t : 2, K_y: 3}
 inv_list = [ 'Copper', 'Copper', 'Copper', 'Aluminum', 'Aluminum', 'Uranium', 'Ruby', 'Ruby', 'Emerald', 'Diamond']
 my_dict = {}
@@ -12,15 +13,17 @@ for material in inv_list:
 print my_dict
 keys = {K_1: 'Copper', K_2: "Aluminum", K_3: "Uranium", K_4: "Copper", K_5:"Aluminum", K_6:"Uranium", K_q: "Ruby", K_w: "Emerald", K_e : "Diamond", K_r : "Ruby", K_t : "Emerald", K_y : "Diamond"}
 pygame.init()
-screen = pygame.display.set_mode((500,500))
+screen_render = pygame.Surface((3840,2160))
+screen_render.fill((255,0,0))
+screen_final = pygame.transform.smoothscale(screen_render, display_res)
+screen = pygame.display.set_mode(display_res)
+screen.blit(screen_final,(0,0))
 pygame.display.update()
 item_type = [0,0,0,0,0]
 item_qtty = [0,0,0,0,0]
 keys_metal = [K_1,K_2,K_3,K_4,K_5,K_6]
 keys_gems = [K_q,K_w,K_e,K_r,K_t,K_y]
 keys_end = [K_RETURN]
-m_tuple = (0,0)
-g_tuple = (0,0)
 foo = True
 def potater (index):
                 if event.key in keys:
@@ -69,6 +72,8 @@ def potater (index):
                                     item_qtty[index] = 0
                                 print keys[event.key], "removed, total amount", item_qtty[index]
                                 print 'amount left:', my_dict[keys[event.key]]
+                return (item_type[index],item_qtty[index])
+                    
 while foo:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -83,7 +88,6 @@ while foo:
                 pass
 #En bas: tree prototype
 dict = {(1,1,1,1) : 'pistol', (1,2,1,1): 'shotgun', (1,3,1,1): 'potato launcher'}
-crafting_list = [item_type[0], item_qtty[0], item_type[1], item_type[1]]
-crafting_list = tuple(crafting_list)
+crafting_list = metal_tuple+gem_tuple
 print dict [crafting_list]
 #we can add more possibilities (more metals, more gems, more stuff (rocks? idk) just need to edit the recipes
